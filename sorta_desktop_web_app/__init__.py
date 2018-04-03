5ca9ee4753ebf0dabf6716b4bf3b43d8564483ba
Ronald Seoh <ronaldseoh@icloud.com>
2018-04-03 18:11:27 +0900

    added __main__.py

--- sorta_desktop_web_app/__init__.py
+++ sorta_desktop_web_app/__init__.py
@@ -1,6 +1,1 @@
 __all__ = ['tray_helper', 'server_controller', 'flask_main']
-
-from . import tray_helper
-
-helper = tray_helper.TrayHelperController()
-helper.start_app()
---
+++ sorta_desktop_web_app/__main__.py
@@ -0,0 +1,4 @@
+from . import tray_helper
+
+helper = tray_helper.TrayHelperController()
+helper.start_app()
