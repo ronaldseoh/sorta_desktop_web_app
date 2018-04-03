e9e991c7efa5fc7ab5bc6133377865a5b8a2252f
Ronald Seoh <ronaldseoh@icloud.com>
2018-04-03 13:14:45 +0900

    make TrayHelperController control everything

--- sorta_desktop_web_app/__init__.py
+++ sorta_desktop_web_app/__init__.py
@@ -1,47 +1,6 @@
-__all__ = ['tray_helper', 'flask_main']
+__all__ = ['tray_helper', 'server_controller', 'flask_main']
 
-import threading
-import socket
-from contextlib import closing
-import webbrowser
-import cherrypy
-
-from . import flask_main
 from . import tray_helper
 
-
-class ServerController(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    @staticmethod
-    def find_free_port():
-        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-            s.bind(('', 0))
-            return s.getsockname()[1]
-
-    def run(self):
-        self.port_number = self.find_free_port()
-
-        cherrypy.tree.graft(flask_main.app, "/")
-
-        cherrypy.config.update(
-            {
-                'server.socket_host': '127.0.0.1',
-                'server.socket_port': self.port_number
-            }
-        )
-
-        cherrypy.engine.start()
-
-        webbrowser.open("http://127.0.0.1:" + str(self.port_number))
-
-    def kill_server(self):
-        cherrypy.engine.exit()
-
-server_thread = ServerController()
-server_thread.start()
-
-helper_thread = tray_helper.TrayHelperController(server_thread)
-helper_thread.start()
+helper = tray_helper.TrayHelperController()
+helper.start_app()
--- sorta_desktop_web_app/tray_helper/tray_windows.py
+++ sorta_desktop_web_app/tray_helper/tray_windows.py
@@ -1,30 +1,36 @@
-import threading
+import webbrowser
 import infi.systray
 
+from ..server_controller import ServerController
 
-class TrayHelperController(threading.Thread):
 
-    def __init__(self, server_thread):
-        threading.Thread.__init__(self)
+class TrayHelperController():
 
-        self.server_thread = server_thread
-
-        self.menu_options = (
+    def __init__(self):
+        self.__menu_options = (
             ("Say Hello", None, self.say_hello),
         )
 
-        self.systray = infi.systray.SysTrayIcon(
+        self.__systray = infi.systray.SysTrayIcon(
             "icon.ico",
             "Example tray icon",
-            self.menu_options,
-            on_quit=self.kill_threads
+            self.__menu_options,
+            on_quit=self.stop_app
         )
+
+        self.__server_controller = ServerController()
 
     def say_hello(self, systray):
         print("Hello, World!")
 
-    def kill_threads(self, systray):
-        self.server_thread.kill_server()
+    def stop_app(self, systray):
+        self.__server_controller.stop_server()
 
-    def run(self):
-        self.systray.start()
+    def start_app(self):
+        # Start tray helper first
+        self.__systray.start()
+
+        # Start the server
+        self._port_number = self.__server_controller.start_server()
+
+        webbrowser.open("http://127.0.0.1:" + str(self._port_number))
