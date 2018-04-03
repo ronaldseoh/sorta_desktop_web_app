1b5724e2e18802633879bbb1cd8168687037ae73
Ronald Seoh <ronaldseoh@icloud.com>
2018-04-03 13:14:19 +0900

    avoid import *

--- sorta_desktop_web_app/tray_helper/__init__.py
+++ sorta_desktop_web_app/tray_helper/__init__.py
@@ -3,6 +3,6 @@ __all__ = ['tray_windows', 'tray_darwin']
 import platform
 
 if platform.system() == 'Windows':
-    from .tray_windows import *
+    from .tray_windows import TrayHelperController
 elif platform.system() == 'Darwin':
-    from .tray_macos import *
+    from .tray_macos import TrayHelperController
